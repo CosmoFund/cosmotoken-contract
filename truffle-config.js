7e9bbeec5a255c2617d4ca68566502e192c77768
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2021-03-09 03:53:27 +0300

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   contracts_directory: './contracts',
-  contracts_build_directory: './abi',
+  contracts_build_directory: './build',
   compilers: {
     solc: {
       version: '0.7.6',
